3f03cfb4a3d96073ed5c1dbd2f95359d4afbf823
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-09-16 15:39:42 +0200

    End of 10 exercise

---
+++ Snack-16092022.Snack1/Program.cs
@@ -0,0 +1,12 @@
+﻿const byte maxInputs = 2;
+List<int> numbers = new();
+
+Console.WriteLine($"Enter {maxInputs} integer numbers:");
+
+for (int i = 0; i < maxInputs; i++)
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    numbers.Add(number);
+}
+
+Console.Write($"The highest number is: {numbers.Max()}");
