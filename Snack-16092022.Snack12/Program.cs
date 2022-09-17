21406fe4d1d4b126dab491ef8cba2c29d8fe6bc0
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-09-17 12:12:05 +0200

    add snack 12

---
+++ Snack-16092022.Snack12/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Please, enter an integer number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(
+    IsEven(number) 
+    ? $"The number {number} is even" 
+    : $"The number {number} is odd");
+
+bool IsEven(int num)
+{
+    return num % 2 == 0;
+}
