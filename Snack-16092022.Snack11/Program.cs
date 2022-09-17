331ba7f31c698f0555f8a3c85d61231c421e8be0
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-09-17 12:09:19 +0200

    add snack 11

---
+++ Snack-16092022.Snack11/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Please, enter a string:");
+string firstStr = Console.ReadLine();
+
+Console.WriteLine("Please, enter another string:");
+string secondStr = Console.ReadLine();
+
+PrintLongest(firstStr, secondStr);
+
+void PrintLongest(string firstString, string secondString)
+{
+    if (firstString.Length == secondString.Length)
+    {
+        Console.WriteLine($"They are equal: {firstStr} {secondStr}");
+        return;
+    }
+
+    string longestString = 
+        firstString.Length > secondString.Length 
+        ? firstString 
+        : secondString;
+    
+    Console.WriteLine($"That's the longest one: {longestString}");
+}
